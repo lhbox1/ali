c4fdc13f5bce13f8bd1456893318c0fd62c7a60b
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2023-11-02 21:32:32 +0800

    Update diy-part1.sh

--- hanwckf/diy-part1.sh
+++ hanwckf/diy-part1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 
-echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
+#echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
 
+#git clone https://github.com/messense/aliyundrive-webdav package/messense
 #更改默认地址为192.168.15.1
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
