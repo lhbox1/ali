2dabe802abb8fa62d603fd84f3bcdee019c7f431
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2023-11-02 22:06:57 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.15.99/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.15.99/g' package/base-files/files/bin/config_generate
 
 # Firewall
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/haproxy feeds/packages/net/v2ray-geodata
+# rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/haproxy feeds/packages/net/v2ray-geodata
 
 # Add packages
-svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
-svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
+# svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
+# svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
